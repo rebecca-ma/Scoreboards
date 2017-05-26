29dcac36989afc5bc85d5d073ce4d83a644f2651
Rebecca Tang <rebecca@malachiarts.com>
2017-05-26 01:07:41 -0700

    redirect back

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -3,7 +3,7 @@ class CaptionsController < ApplicationController
     verify_user {
       @photo = Photo.find(params[:photo_id])
       if @curr_user != @photo.user
-        redirect back
+        redirect_to back
       else
         @caption = @photo.caption
       end
@@ -14,7 +14,7 @@ class CaptionsController < ApplicationController
     verify_user {
       @photo = Photo.find(params[:photo_id])
       if @curr_user != @photo.user
-        redirect back
+        redirect_to back
       else
         @caption = @photo.caption
         if @caption.update(caption_params)
